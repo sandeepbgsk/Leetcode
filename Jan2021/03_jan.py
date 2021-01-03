5d7b02dba9030c24e1a4b6faac350290ae31c0ad
bhgsk <bhgsks@gmail.com>
2021-01-03 23:17:39 +0530

    Fibo test

--- Jan2021/03_jan.py
+++ Jan2021/03_jan.py
@@ -2,7 +2,7 @@ from itertools import permutations
 
 
 class Solution:
-    def countArrangement(self, n: int) -> int:
+    def countArrangement2(self, n: int) -> int:
         count = 0
         mylist = range(1,n+1)
         perm = list(permutations(mylist))
@@ -15,9 +15,24 @@ class Solution:
                     flag = False
                     break
             if flag:
+                print(i)
                 count = count + 1
 
         return count
 
+    def countArrangement(self, n: int) -> int:
+        if(n==1):
+           return 1
+        if(n==2):
+            return 2
+        count = 0
+        fibo_list = [1,2]
+        for i in range(3,n+1):
+            fibo_list.append(fibo_list[i-2] + fibo_list[i-3])
+
+        return fibo_list[-1]
+
+
 if __name__ == '__main__':
-    print(Solution().countArrangement(n=15))
+    print(Solution().countArrangement(n=4))
+    print(Solution().countArrangement2(n=4))
